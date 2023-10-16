1bafc16a43dc0d51b236705c4411edc07d43e134
Kevin De La Bastide <kevindelabastide@gmail.com>
2023-10-16 18:56:15 -0400

    Initial Push to GitHub

---
+++ variable.js
@@ -0,0 +1,18 @@
+let i;
+for (i = 18; i > 5; i--) {
+    console.log(i);
+}
+console.log(i);
+console.log("-------------------------------------------");
+do {
+    i += 10; 
+    console.log(i);
+
+}
+  while (i < 5);
+  console.log(i);
+  console.log("-------------------------------------------");
+
+  while (i > 3);
+  console.log(i);
+  console.log("-------------------------------------------");
